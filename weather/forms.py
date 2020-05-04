9dd3a4d3e9603b22a5a0408fe5df0a3f312db442
ravinduf <ravinduf@rocketmail.com>
2020-05-04 14:59:01 +0530

    added widgets for form

--- weather/forms.py
+++ weather/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 from .models import *
 
@@ -8,3 +8,4 @@ class cityForm(forms.ModelForm):
     class Meta:
         model = City
         fields = ('name',)
+        widgets = {'name': TextInput(attrs={'class': 'input', 'placeholder': 'City Name'})}
