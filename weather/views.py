658e94e49eb28bfbae13a93841de510db6db733a
ravinduf <ravinduf@rocketmail.com>
2020-05-04 14:59:49 +0530

    updated index.html and fixed errors in views.py

--- weather/views.py
+++ weather/views.py
@@ -8,9 +8,8 @@ from .forms import *
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=4ada227d2084bef19fb1c459fadd44a6'
     
-
     cities = City.objects.all()
-
+    
     form = cityForm()
 
     if request.method == 'POST':
@@ -20,17 +19,21 @@ def index(request):
 
 
     weather_data = []
+    
+
 
     for city in cities:
         r = requests.get(url.format(city)).json()
-        city_weather = {
-            'city': city.name,
-            'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon']
-        }
-        weather_data.append(city_weather)
-
+        try:
+            city_weather = {
+                'city': city.name,
+                'temperature': r['main']['temp'],
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon']
+            }
+            weather_data.append(city_weather)
+        except:
+            pass
     
     context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/index.html', context)
