92820b7903714d364ef5102e81b1ebb8a5776957
ravinduf <ravinduf@rocketmail.com>
2020-05-04 14:44:08 +0530

    updated index view for forms

--- weather/views.py
+++ weather/views.py
@@ -1,22 +1,36 @@
 import requests 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from .forms import *
 
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=4ada227d2084bef19fb1c459fadd44a6'
-    city = 'Kandana'
-
-    r = requests.get(url.format(city)).json()
     
 
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon']
-    }
+    cities = City.objects.all()
 
-    print(city_weather)
-    context = {'city_weather': city_weather}
+    form = cityForm()
+
+    if request.method == 'POST':
+        form = cityForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+
+    weather_data = []
+
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+        weather_data.append(city_weather)
+
+    
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/index.html', context)
